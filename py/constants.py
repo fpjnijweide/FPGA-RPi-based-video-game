8a57371e04aa60ff7a0511f17e8b3bb8bbd83e0c
Daan Dieperink <d.dieperink@student.utwente.nl>
2018-10-09 16:51:59 +0200

    constants file

---
+++ py/constants.py
@@ -0,0 +1,16 @@
+#Global variables should be defined here
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+BLACK    = (  0,   0,   0)
+COMBLUE  = (233, 232, 255)
