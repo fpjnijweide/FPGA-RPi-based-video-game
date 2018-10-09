87157cef162c28d5190f57b9633c3f66eed8a2cd
Daan Dieperink <d.dieperink@student.utwente.nl>
2018-10-09 15:45:36 +0200

    pygametest stuff

--- py/pygametest.py
+++ py/pygametest.py
@@ -3,11 +3,12 @@ from pygame.locals import *
 
 pygame.init()
 
-windowSurface = pygame.display.set_mode((800, 600), 0, 32)
-pygame.display.set_caption('Does it work?')
+windowSurface = pygame.display.set_mode((800, 600), 0, 32) # (Xres, Yres), flags, depth
+pygame.display.set_caption('Does it work?') # window name
 
 pygame.display.update()
 
+# note: __name__ != '__main__' when the program is imported
 while __name__ == '__main__':
     for event in pygame.event.get():
         if event.type == QUIT:
