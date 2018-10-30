df8427469695ff183eb01d9f950cc3485f204283
fpjnijweide <fpjnijweide@protonmail.com>
2018-10-30 17:22:43 +0100

    we are now able to send data

--- py/connection_readsend.py
+++ py/connection_readsend.py
@@ -8,57 +8,88 @@ p.nice(-19)
 
 pi = pigpio.pi('Dragon47')
 
-#GPIO.cleanup()
-##GPIO.setmode(GPIO.BOARD)
 
-#GPIO.setup(23, GPIO.OUT) #clock will be on pin 23
-#clock = GPIO.PWM(23, 500) # hz
-#clock.start(50) # start the clock witha  duty cycle of 50%
 
 pi.set_PWM_frequency(11, 1000)
-pi.set_PWM_dutycycle(11,110)
-#GPIO.setup(19, GPIO.OUT) #slave select will be on pin 24
+pi.set_PWM_dutycycle(11,127)
 
-#GPIO.setup(40, GPIO.IN) #pin 40 used for input
-#GPIO.setup(38, GPIO.OUT) #pin 38 used for output
 
 pi.write(10, 1)
-#GPIO.output(19, select)
 
-def readinfo(pin):
+
+
+
+
+
+#WRITE THE VALUE YOU WANT TO SEND HERE
+send_value=9
+
+
+
+
+
+
+
+
+
+
+
+
+
+send_value_bin=bin(send_value)[2:]
+
+if len(send_value_bin) > 8:
+    print("overflow")
+
+if len(send_value_bin)<8:
+    diff=8-len(send_value_bin)
+    zerolist=diff*['0']
+    zerostring=''.join(zerolist)
+    send_value_bin=zerostring+send_value_bin
+
+def readinfo(pin,i):
     res = []
     cycles = 0
-    #TODO find out if there is a neater way to detect clock than read pin
+    #TODO find out if there is a neater way to detect clock than read pin 11
+
     clock_value_previous = pi.read(11) #GPIO.input(23)
     
+
+
+    print(send_value_bin)
     while True:
         clock_value_now = pi.read(11) #GPIO.input(23)
         if (clock_value_now != clock_value_previous and clock_value_now == 1):
-            #cycles += 1
-            #GPIO.output(19, 0)
-            #res.append(GPIO.input(pin))
+            
+            if cycles <=7:
+                writecycle=send_value_bin[cycles]
+                pi.write(25, int(writecycle))
+
             pi.write(10, 0)
             res.append(pi.read(pin))
-            
-            pi.write(25, 1)
+            #pi.write(24, 0)
+            #pi.write(23, 0)
+
+            cycles += 1
             
         if (len(res) == 9):
             pi.write(10, 1)
-            #GPIO.output(19, 1)
-            #sleep(0.1)
             
+
             break
         clock_value_previous = clock_value_now
     return res
     
-        
-for i in range(50):
-    print(readinfo(16))
+
+for i in range(60):
+    print(readinfo(16,i))
+
     #select += 1
     #sleep(0.1)
 print("DONE")
+pi.write(25,0)
 pi.stop()
 
 
-	
-	
+    
+    
