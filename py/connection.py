7733ffd1303bca23a93320a725df7559eb7beb51
fpjnijweide <fpjnijweide@protonmail.com>
2018-10-31 16:30:54 +0100

    Completely rewrote connection to allow for sending and receiving 8 bits. Also rewrote to use functions that are only called when ran directly.

--- py/connection.py
+++ py/connection.py
@@ -2,33 +2,7 @@ import psutil, os
 import pigpio
 import constants
 import chewnumber
-
-#Start Daemon
 from subprocess import call
-call(["sudo", "pigpiod"])
-
-#STARTING PROCESS
-p = psutil.Process(os.getpid()) #make process high priority
-p.nice(-19)
-pi = pigpio.pi('Dragon47')
-
-#Starting clock
-pi.set_PWM_frequency(constants.CLOCK_PIN, constants.CLOCKSPEED)
-pi.set_PWM_dutycycle(constants.CLOCK_PIN, constants.DUTYCYCLE)
-
-pi.write(constants.MOSI_PIN, 1) #make sure no data is sent yet
-
-# WRITE THE VALUES YOU WANT TO SEND HERE
-xspeed=2
-yspeed=3
-bounciness=1.125
-isvertical=True
-
-#Converting data to binary
-data=list(map(chewnumber.decToFixedPoint,[xspeed,yspeed,bounciness,isvertical]))
-
-
-#########################################
 
 # Function definitions
 
@@ -38,47 +12,76 @@ def readyToReceive():
 def notReadyToReceive():
     pi.write(constants.MOSI_PIN, 1)
 
-def writeCycle(cycles,data):
+def writeBit(cycles,data):
+    # TODO switch to threads instead of writing/reading sequentially
     i=0
     for key,currentPin in constants.WRITE_PINS.items():
-        if cycles <=7:
-            currentBit=data[i][cycles]
-            pi.write(currentPin, int(currentBit))
+
+        currentBit=data[i][cycles]
+        pi.write(currentPin, int(currentBit))
         i+=1
 
-def rwbyte(pin):
-    res = []
-    cycles = 0
-    #TODO switch to threads instead of writing/reading sequentially
+def readBit(readPin):
+    # TODO switch to threads instead of writing/reading sequentially
+    bitList=[]
+    for key, currentPin in constants.READ_PINS.items():
+        bitList.append(pi.read(currentPin))
+    return bitList
 
-    clock_value_previous = pi.read(11)
+def rwByte(xspeed,yspeed,bounciness,isvertical):
+    # Converting data to binary
+    data = list(map(chewnumber.decToFixedPoint, [xspeed, yspeed, bounciness, isvertical]))
 
-    while True:
-        clock_value_now = pi.read(11)
-        if (clock_value_now != clock_value_previous and clock_value_now == 1):
-            if cycles==0:
+    receivedData = []
+    currentCycle = 0
+
+    previousClock = pi.read(constants.CLOCK_PIN)
+
+    while len(res) < 9:
+        currentClock = pi.read(constants.CLOCK_PIN)
+        if (currentClock != previousClock and currentClock == 1):
+            #TODO check if sleeps are needed to allow for game rendering, higher fps
+            if currentCycle==0:
                 readyToReceive()
+            if currentCycle <= 7:
+                writeBit(currentCycle,data)
+            if currentCycle>=1
+                receivedBits=readBit()
+                receivedData.append(receivedBits)
+            currentCycle += 1
 
-            writeCycle(cycles,data)
+        previousClock = currentClock
 
-            res.append(pi.read(pin))
-            cycles += 1
-            
-            if (len(res) == 9):
-                notReadyToReceive()
-                break
-        clock_value_previous = clock_value_now
-    return res
+    notReadyToReceive()
+    return receivedData
     
+def initConnection():
+    #START DAEMON
+    call(["sudo", "pigpiod"])
+
+    # STARTING PROCESS, SET PRIORITY
+    p = psutil.Process(os.getpid())  # make process high priority
+    p.nice(-19)
+    pi = pigpio.pi('Dragon47')
+
+    # Starting clock
+    pi.set_PWM_frequency(constants.CLOCK_PIN, constants.CLOCKSPEED)
+    pi.set_PWM_dutycycle(constants.CLOCK_PIN, constants.DUTYCYCLE)
+
+    pi.write(constants.MOSI_PIN, 1)  # make sure no data is sent yet
 
 def main():
-    for i in range(60):
-        print(rwbyte(16))
+    initConnection()
+    # WRITE THE VALUES YOU WANT TO SEND HERE
+    xspeed = 2
+    yspeed = 3
+    bounciness = 1.125
+    isvertical = True
 
-        # select += 1
-        # sleep(0.1)
+    for i in range(60):
+        returnval=rwByte(xspeed,yspeed,bounciness,isvertical)
+        print(returnval)
     print("DONE")
-    pi.write(25, 0)
     pi.stop()
 
 if __name__ == "__main__":
