562e87e9f27034c6a1689c76fc3342439c1cae42
Catalin Rus <a.c.rus@student.utwente.nl>
2018-10-31 10:38:40 +0100

    added funcitonality for sending 3 values at once

--- py/connection_readsend.py
+++ py/connection_readsend.py
@@ -1,42 +1,74 @@
 from time import sleep
+#import RPi.GPIO as GPIO
 import psutil, os
 import pigpio
 
-def initialise():
-    p = psutil.Process(os.getpid())
-    p.nice(-19)
-    pi = pigpio.pi('Dragon47')
-    pi.set_PWM_frequency(11, 1000)
-    pi.set_PWM_dutycycle(11,128)
+p = psutil.Process(os.getpid())
+p.nice(-19)
+
+pi = pigpio.pi('Dragon47')
+
+
+
+pi.set_PWM_frequency(11, 1000)
+pi.set_PWM_dutycycle(11,127)
+
 
 pi.write(10, 1)
 
+
+
+
+
+
 #WRITE THE VALUE YOU WANT TO SEND HERE
-send_value=9
+send_value[0]=9
+send_value[1]=8
+send_value[2]=7
 
-send_value_bin=bin(send_value)[2:]
 
-if len(send_value_bin) > 8:
-    print("overflow")
 
-if len(send_value_bin)<8:
-    diff=8-len(send_value_bin)
-    zerolist=diff*['0']
-    zerostring=''.join(zerolist)
-    send_value_bin=zerostring+send_value_bin
 
-def readinfo(pin):
+
+
+
+
+
+
+
+for i in range(0,3):
+    send_value_bin[i]=bin(send_value[i])[2:]
+
+    if len(send_value_bin[i]) > 8:
+        print("overflow")
+
+    if len(send_value_bin[i])<8:
+        diff[i]=8-len(send_value_bin[i])
+        zerolist[i]=(diff[i])*['0']
+        zerostring[i]=''.join(zerolist[i])
+        send_value_bin[i]=zerostring[i]+send_value_bin[i]
+
+def readinfo(pin,i):
     res = []
     cycles = 0
     #TODO find out if there is a neater way to detect clock than read pin 11
-    clock_value_previous = pi.read(11)
+
+    clock_value_previous = pi.read(11) #GPIO.input(23)
+    
+
+
+    print(send_value_bin)
     while True:
         clock_value_now = pi.read(11) #GPIO.input(23)
         if (clock_value_now != clock_value_previous and clock_value_now == 1):
-            
+
+
+            for i in range(0,3)
             if cycles <=7:
-                writecycle=send_value_bin[cycles]
-                pi.write(25, int(writecycle))
+                writecycle=send_value_bin[i][cycles]
+                pi.write(23+i, int(writecycle))
+            else:
+
 
             pi.write(10, 0)
             res.append(pi.read(pin))
@@ -44,15 +76,21 @@ def readinfo(pin):
             #pi.write(23, 0)
 
             cycles += 1
+            
         if (len(res) == 9):
             pi.write(10, 1)
+            
+
             break
         clock_value_previous = clock_value_now
     return res
+    
 
-initialise()
 for i in range(60):
     print(readinfo(16))
+
+    #select += 1
+    #sleep(0.1)
 print("DONE")
 pi.write(25,0)
 pi.stop()
