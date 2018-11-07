c570300a4192d82a0c4631f9dd656a8c83d9ffee
Daan Dieperink <d.dieperink@student.utwente.nl>
2018-11-07 12:24:52 +0100

    Name now cannot be empty or contain only spaces

--- py/main.py
+++ py/main.py
@@ -648,6 +648,9 @@ class MainMenu:
 
             global PlayerName
             PlayerName = self.textinput.get_text()
+            if PlayerName.strip() == '':
+                PlayerName = 'Player'
+
             pygame.event.clear()
 
             self.textinput.set_cursor_color(constants.colors['BLACK'])
@@ -878,11 +881,6 @@ class Audio:
             return
 
         self.gameSounds[soundName].play(0)
-
-    def playandduck(self, soundName):
-
-        if not constants.SOUND:
-            return
 
 
 # Execute init() and main() only when program is run directly (not imported)
